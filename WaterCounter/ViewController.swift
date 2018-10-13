d8728e0b536db9f6af063099452bf8d0eeb2bd7d
Isaac R <izaak@live.co.kr>
2018-10-13 10:54:09 -0500

    Initial Commit

---
+++ WaterCounter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WaterCounter
+//
+//  Created by Rodriguez Isaac on 10/13/18.
+//  Copyright © 2018 RodriguezIsaac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
